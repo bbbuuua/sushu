933740a9a6920a162369f66d9a93673cf8bd6433
bbbuuua <103034193+bbbuuua@users.noreply.github.com>
2022-04-10 23:52:51 +0800

    Create sushu1.c

---
+++ sushu1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int fun(int n)
+{
+	int i;
+	for (i = 2; i < n; i++)
+	
+		if (n%i == 0)
+		{
+		
+			return 0;
+		}
+		
+			return 1;
+	
+}
+void main()
+{
+	int d,n=0, i;
+	printf("输入正整数n：");
+	scanf_s("%d", &n);
+	for (i = 2; i < n; i++)
+	{
+
+		d = fun(i);
+		if (d)
+		printf("%d\n", i);
+
+	}
+}
